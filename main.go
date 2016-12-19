b32aeb37f408e6931e22bddef2f8d74fb7eb9b71
Carlo Strub <cs@carlostrub.ch>
2016-12-19 22:02:35 +0000

    implement first version of paas.Count

--- main.go
+++ main.go
@@ -2,13 +2,33 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
+
+	"github.com/kolo/xmlrpc"
 )
 
 func main() {
 
 	apiKey := os.Getenv("GANDI_KEY")
 
-	fmt.Println(apiKey)
+	//	api, err := xmlrpc.NewClient("https://rpc.gandi.net/xmlrpc/", nil)
+	api, err := xmlrpc.NewClient("https://rpc.ote.gandi.net/xmlrpc/", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//	var result struct {
+	//		Count int `xmlrpc:"count"`
+	//	}
+
+	// Count number of instances
+	var paasCount *int
+	err = api.Call("paas.count", apiKey, &paasCount)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(*paasCount)
 
 }
