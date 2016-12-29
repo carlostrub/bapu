da534248c2b1b1c7dd669dfe21eab9eb971be101
Carlo Strub <cs@carlostrub.ch>
2016-12-29 20:59:42 +0000

    add more fields

--- main.go
+++ main.go
@@ -82,18 +82,44 @@ func main() {
 
 	termui.Render(count)
 
-	// List instances
+	// Define output structs
+	type DiskReturn struct {
+		CanSnapshot   bool      `xmlrpc:"can_snapshot"`
+		DatacenterID  int       `xmlrpc:"datacenter_id"`
+		DateCreated   time.Time `xmlrpc:"date_created"`
+		DateUpdated   time.Time `xmlrpc:"date_updated"`
+		ID            int       `xmlrpc:"id"`
+		IsBootDisk    bool      `xmlrpc:"is_boot_disk"`
+		KernelVersion string    `xmlrpc:"kernel_version"`
+		Label         string    `xmlrpc:"label"`
+		Name          string    `xmlrpc:"name"`
+		Size          int       `xmlrpc:"size"`
+		State         string    `xmlrpc:"state"`
+		TotalSize     int       `xmlrpc:"total_size"`
+		Type          string    `xmlrpc:"type"`
+		Visibility    string    `xmlrpc:"visibility"`
+	}
 	type VMReturn struct {
-		AiActive     int       `xmlrpc:"ai_active"`
-		Console      int       `xmlrpc:"console"`
-		Cores        int       `xmlrpc:"cores"`
-		DatacenterID int       `xmlrpc:"datacenter_id"`
-		DateCreated  time.Time `xmlrpc:"date_created"`
-		DateUpdated  time.Time `xmlrpc:"date_updated"`
-		Description  string    `xmlrpc:"description"`
-		Hostname     string    `xmlrpc:"hostname"`
+		AiActive     int          `xmlrpc:"ai_active"`
+		Console      int          `xmlrpc:"console"`
+		ConsoleURL   string       `xmlrpc:"console_url"`
+		Cores        int          `xmlrpc:"cores"`
+		DatacenterID int          `xmlrpc:"datacenter_id"`
+		DateCreated  time.Time    `xmlrpc:"date_created"`
+		DateUpdated  time.Time    `xmlrpc:"date_updated"`
+		Description  string       `xmlrpc:"description"`
+		Disks        []DiskReturn `xmlrpc:"disks"`
+		Farm         string       `xmlrpc:"farm"`
+		FlexShares   int          `xmlrpc:"flex_shares"`
+		Hostname     string       `xmlrpc:"hostname"`
+		HVMState     string       `xmlrpc:"hvm_state"`
+		ID           int          `xmlrpc:"id"`
+		Memory       int          `xmlrpc:"memory"`
+		State        string       `xmlrpc:"state"`
+		VMmaxMemory  int          `xmlrpc:"vm_max_memory"`
 	}
 
+	// List instances
 	var hostingVMList *[]VMReturn
 	err = api.Call("hosting.vm.list", apiKey, &hostingVMList)
 	if err != nil {
@@ -103,7 +129,7 @@ func main() {
 	var strs []string
 	list := *hostingVMList
 	for _, val := range list {
-		strs = append(strs, val.Hostname)
+		strs = append(strs, val.Hostname+" ("+val.State+")")
 	}
 
 	ls := termui.NewList()
