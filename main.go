305236d0dbbad56f4bd1523c822e0715d10cd9c6
Carlo Strub <cs@carlostrub.ch>
2017-01-01 21:11:32 +0000

    use tables, rearrange things, move struct definitions out of main(), etc.

--- main.go
+++ main.go
@@ -12,6 +12,58 @@ import (
 	"github.com/spf13/viper"
 )
 
+var selector int
+
+// AccountReturn contains fields for informations about the Gandi account
+type AccountReturn struct {
+	AverageCreditCost     float64   `xmlrpc:"average_credit_cost"`
+	Credits               int       `xmlrpc:"credits"`
+	CycleDay              int       `xmlrpc:"cycle_day"`
+	DateCreditsExpiration time.Time `xmlrpc:"date_credits_expiration"`
+	FullName              string    `xmlrpc:"fullname"`
+	Handle                string    `xmlrpc:"handle"`
+	ID                    int       `xmlrpc:"id"`
+}
+
+// DiskReturn contains fields for informations about the Disks
+type DiskReturn struct {
+	CanSnapshot   bool      `xmlrpc:"can_snapshot"`
+	DatacenterID  int       `xmlrpc:"datacenter_id"`
+	DateCreated   time.Time `xmlrpc:"date_created"`
+	DateUpdated   time.Time `xmlrpc:"date_updated"`
+	ID            int       `xmlrpc:"id"`
+	IsBootDisk    bool      `xmlrpc:"is_boot_disk"`
+	KernelVersion string    `xmlrpc:"kernel_version"`
+	Label         string    `xmlrpc:"label"`
+	Name          string    `xmlrpc:"name"`
+	Size          int       `xmlrpc:"size"`
+	State         string    `xmlrpc:"state"`
+	TotalSize     int       `xmlrpc:"total_size"`
+	Type          string    `xmlrpc:"type"`
+	Visibility    string    `xmlrpc:"visibility"`
+}
+
+// VMReturn contains fields for informations about the virtual machines
+type VMReturn struct {
+	AiActive     int          `xmlrpc:"ai_active"`
+	Console      int          `xmlrpc:"console"`
+	ConsoleURL   string       `xmlrpc:"console_url"`
+	Cores        int          `xmlrpc:"cores"`
+	DatacenterID int          `xmlrpc:"datacenter_id"`
+	DateCreated  time.Time    `xmlrpc:"date_created"`
+	DateUpdated  time.Time    `xmlrpc:"date_updated"`
+	Description  string       `xmlrpc:"description"`
+	Disks        []DiskReturn `xmlrpc:"disks"`
+	Farm         string       `xmlrpc:"farm"`
+	FlexShares   int          `xmlrpc:"flex_shares"`
+	Hostname     string       `xmlrpc:"hostname"`
+	HVMState     string       `xmlrpc:"hvm_state"`
+	ID           int          `xmlrpc:"id"`
+	Memory       int          `xmlrpc:"memory"`
+	State        string       `xmlrpc:"state"`
+	VMmaxMemory  int          `xmlrpc:"vm_max_memory"`
+}
+
 // LoadAPI returns the api and apiKey according to the settings defined in the
 // configuration file, respectively.
 func LoadAPI() (api *xmlrpc.Client, apiKey string, err error) {
@@ -53,6 +105,35 @@ func LoadAPI() (api *xmlrpc.Client, apiKey string, err error) {
 	return api, apiKey, nil
 }
 
+func serverList(list []VMReturn) (servers [][]string) {
+
+	servers = append(servers, []string{
+		"Selected",
+		"Hostname",
+		"Datacenter",
+		"Cores",
+		"Memory",
+		"State",
+	})
+
+	for i, val := range list {
+		s := ""
+		if selector == i {
+			s = "*"
+		}
+		servers = append(servers, []string{
+			s,
+			val.Hostname,
+			strconv.Itoa(val.DatacenterID),
+			strconv.Itoa(val.Cores),
+			strconv.Itoa(val.Memory) + "MB",
+			val.State,
+		})
+	}
+
+	return servers
+}
+
 func main() {
 	// Load API
 	api, apiKey, err := LoadAPI()
@@ -69,56 +150,29 @@ func main() {
 
 	// Title
 	uiTitle := termui.NewPar("Bapu -- Control your Gandi Machines")
-	uiTitle.Border = true
+	uiTitle.Border = false
 	uiTitle.Height = 3
 	uiTitle.TextFgColor = termui.ColorMagenta
 
-	// Count number of instances
+	// Summary
 	var hostingVMCount *int
 	err = api.Call("hosting.vm.count", apiKey, &hostingVMCount)
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	uiCount := termui.NewPar("VM #: " + strconv.Itoa(*hostingVMCount))
-	uiCount.TextFgColor = termui.ColorWhite
-
-	// Define output structs
-	type DiskReturn struct {
-		CanSnapshot   bool      `xmlrpc:"can_snapshot"`
-		DatacenterID  int       `xmlrpc:"datacenter_id"`
-		DateCreated   time.Time `xmlrpc:"date_created"`
-		DateUpdated   time.Time `xmlrpc:"date_updated"`
-		ID            int       `xmlrpc:"id"`
-		IsBootDisk    bool      `xmlrpc:"is_boot_disk"`
-		KernelVersion string    `xmlrpc:"kernel_version"`
-		Label         string    `xmlrpc:"label"`
-		Name          string    `xmlrpc:"name"`
-		Size          int       `xmlrpc:"size"`
-		State         string    `xmlrpc:"state"`
-		TotalSize     int       `xmlrpc:"total_size"`
-		Type          string    `xmlrpc:"type"`
-		Visibility    string    `xmlrpc:"visibility"`
+	var hostingAccountInfo *AccountReturn
+	err = api.Call("hosting.account.info", apiKey, &hostingAccountInfo)
+	if err != nil {
+		log.Fatal(err)
 	}
-	type VMReturn struct {
-		AiActive     int          `xmlrpc:"ai_active"`
-		Console      int          `xmlrpc:"console"`
-		ConsoleURL   string       `xmlrpc:"console_url"`
-		Cores        int          `xmlrpc:"cores"`
-		DatacenterID int          `xmlrpc:"datacenter_id"`
-		DateCreated  time.Time    `xmlrpc:"date_created"`
-		DateUpdated  time.Time    `xmlrpc:"date_updated"`
-		Description  string       `xmlrpc:"description"`
-		Disks        []DiskReturn `xmlrpc:"disks"`
-		Farm         string       `xmlrpc:"farm"`
-		FlexShares   int          `xmlrpc:"flex_shares"`
-		Hostname     string       `xmlrpc:"hostname"`
-		HVMState     string       `xmlrpc:"hvm_state"`
-		ID           int          `xmlrpc:"id"`
-		Memory       int          `xmlrpc:"memory"`
-		State        string       `xmlrpc:"state"`
-		VMmaxMemory  int          `xmlrpc:"vm_max_memory"`
-	}
+
+	info := *hostingAccountInfo
+
+	uiSummary := termui.NewPar("Owner: " + info.FullName + "    Virtual Machines: " + strconv.Itoa(*hostingVMCount) + "    Remaining Credit: " + strconv.Itoa(info.Credits) + ":::" + strconv.Itoa(selector))
+	uiSummary.Height = 3
+	uiSummary.Border = true
+	uiSummary.BorderLabel = "Summary"
+	uiSummary.TextFgColor = termui.ColorWhite
 
 	// List instances
 	var hostingVMList *[]VMReturn
@@ -127,17 +181,52 @@ func main() {
 		log.Fatal(err)
 	}
 
-	var strs []string
 	list := *hostingVMList
-	for _, val := range list {
-		strs = append(strs, "["+strconv.Itoa(val.ID)+"] "+val.Hostname+" ("+val.State+")")
+	servers := serverList(list)
+
+	uiTable := termui.NewTable()
+	uiTable.Rows = servers
+	uiTable.FgColor = termui.ColorWhite
+	uiTable.BgColor = termui.ColorDefault
+	uiTable.TextAlign = termui.AlignCenter
+	uiTable.Seperator = false
+	uiTable.Analysis()
+	uiTable.SetSize()
+	uiTable.BgColors[0] = termui.ColorWhite
+	uiTable.FgColors[0] = termui.ColorBlack
+	uiTable.Y = 20
+	uiTable.X = 0
+	uiTable.Border = false
+	for i := 0; i < len(list); i++ {
+		switch list[i].State {
+		case "paused":
+			uiTable.BgColors[i+1] = termui.ColorBlack
+			uiTable.FgColors[i+1] = termui.ColorWhite
+		case "running":
+			uiTable.BgColors[i+1] = termui.ColorBlue
+			uiTable.FgColors[i+1] = termui.ColorWhite
+		case "halted":
+			uiTable.BgColors[i+1] = termui.ColorYellow
+			uiTable.FgColors[i+1] = termui.ColorBlack
+		case "locked":
+			uiTable.BgColors[i+1] = termui.ColorMagenta
+			uiTable.FgColors[i+1] = termui.ColorGreen
+		case "being_created":
+			uiTable.BgColors[i+1] = termui.ColorWhite
+			uiTable.FgColors[i+1] = termui.ColorBlack
+		case "deleted":
+			uiTable.BgColors[i+1] = termui.ColorBlack
+			uiTable.FgColors[i+1] = termui.ColorRed
+		}
 	}
 
-	uiList := termui.NewList()
-	uiList.Items = strs
-	uiList.ItemFgColor = termui.ColorYellow
-	uiList.BorderLabel = "Servers"
-	uiList.Height = len(strs) + 2
+	// Commands
+	uiCommands := termui.NewPar("<[S]tart>    <St[o]p>    <[R]eboot>")
+	uiCommands.Height = 3
+	uiCommands.Border = false
+	uiCommands.BorderLabel = "Summary"
+	uiCommands.TextFgColor = termui.ColorBlack
+	uiCommands.TextBgColor = termui.ColorWhite
 
 	// Create termui Grid system
 	termui.Body.AddRows(
@@ -145,10 +234,13 @@ func main() {
 			termui.NewCol(12, 0, uiTitle),
 		),
 		termui.NewRow(
-			termui.NewCol(2, 0, uiCount),
+			termui.NewCol(12, 0, uiSummary),
 		),
 		termui.NewRow(
-			termui.NewCol(10, 0, uiList),
+			termui.NewCol(12, 0, uiTable),
+		),
+		termui.NewRow(
+			termui.NewCol(12, 0, uiCommands),
 		),
 	)
 
@@ -162,7 +254,18 @@ func main() {
 	})
 
 	termui.Handle("/sys/kbd/<up>", func(termui.Event) {
-		termui.Body.Align()
+		if selector > 0 {
+			selector--
+		}
+		servers = serverList(list)
+		termui.Render(termui.Body)
+	})
+
+	termui.Handle("/sys/kbd/<down>", func(termui.Event) {
+		if selector < len(list) {
+			selector++
+		}
+		servers = serverList(list)
 		termui.Render(termui.Body)
 	})
 
